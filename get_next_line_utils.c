c2bf71d7a59cfe2edee5d9350000e752e52d18f2
Alessandro Assis <aledev21@gmail.com>
2024-12-11 07:16:36 -0300

    fix utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,36 +11,3 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-int mini_popen(const char *command) {
-    int pipe_fd[2];
-    
-    // Cria o pipe
-    if (pipe(pipe_fd) == -1) {
-        return -1;
-    }
-    
-    pid_t pid = fork();
-    if (pid == -1) {
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-        return -1;
-    } else if (pid == 0) { 
-        // Processo filho
-        close(pipe_fd[0]); 
-
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        close(pipe_fd[1]);
-
-        execlp("sh", "sh", "-c", command, (char *) NULL);
-        
-        _exit(1);
-    }
-    
-    // Processo pai
-    close(pipe_fd[1]); 
-    return pipe_fd[0];
-}
